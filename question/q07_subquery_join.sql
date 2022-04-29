5c7232d454c78c8fb73bb9430ff863708e94e86f
chickenpop <>
2022-04-29 22:04:08 +0900

    q07 1, 2, 3 해결 진행도 9/36

--- question/q07_subquery_join.sql
+++ question/q07_subquery_join.sql
@@ -1,33 +1,64 @@
--- employees. 'Munich' 도시에 위치한 부서에 소속된 직원들 명단?
+-- 9/36
+-- 1. employees. 'Munich' 도시에 위치한 부서에 소속된 직원들 명단?
+select 
+    e.*
+from employees e
+    inner join departments d
+        on e.department_id = d.department_id
+            inner join locations l
+                on d.location_id = l.location_id
+                    where l.city = 'Munich';
 
 
-
--- tblMan. tblWoman. 서로 짝이 있는 사람 중 남자와 여자의 정보를 모두 가져오시오.
+-- 2. tblMan. tblWoman. 서로 짝이 있는 사람 중 남자와 여자의 정보를 모두 가져오시오.
 --    [남자]        [남자키]   [남자몸무게]     [여자]    [여자키]   [여자몸무게]
 --    홍길동         180       70              장도연     177        65
 --    아무개         175       null            이세영     163        null
 --    ..
+select * from tblMan;
+select * from tblWoman;
+
+select 
+    m.name as 남자,
+    m.height as 남자키,
+    m.weight as 남자몸무게,
+    w.name as 여자,
+    w.height as 여자키,
+    w.weight as 여자몸무게
+from tblMan m
+    inner join tblWoman w
+        on m.couple = w.name;
+
+
+-- 3. tblAddressBook. 가장 많은 사람들이 가지고 있는 직업은 주로 어느 지역 태생(hometown)인가? 
+select * from tblAddressBook;
+
+select max(count(*)) from tblAddressBook group by job; -- 학생(332명)
+
+select job from tblAddressBook group by job having count(*) = (select max(count(*)) from tblAddressBook group by job);
+
+select 
+    hometown, 
+    count(*) as 분포수
+from tblAddressBook 
+    where job = (select job from tblAddressBook group by job having count(*) = (select max(count(*)) from tblAddressBook group by job)) 
+        group by hometown
+            order by count(*) desc;
+
+-- 4. tblAddressBook. 이메일 도메인들 중 평균 아이디 길이가 가장 긴 이메일 사이트의 도메인은 무엇인가?
 
 
 
--- tblAddressBook. 가장 많은 사람들이 가지고 있는 직업은 주로 어느 지역 태생(hometown)인가?
+-- 5. tblAddressBook. 평균 나이가 가장 많은 출신(hometown)들이 가지고 있는 직업 중 가장 많은 직업은?
 
 
-
--- tblAddressBook. 이메일 도메인들 중 평균 아이디 길이가 가장 긴 이메일 사이트의 도메인은 무엇인가?
-
-
-
--- tblAddressBook. 평균 나이가 가장 많은 출신(hometown)들이 가지고 있는 직업 중 가장 많은 직업은?
-
-
--- tblAddressBook. 남자 평균 나이보다 나이가 많은 서울 태생 + 직업을 가지고 있는 사람들을 가져오시오.
+-- 6. tblAddressBook. 남자 평균 나이보다 나이가 많은 서울 태생 + 직업을 가지고 있는 사람들을 가져오시오.
 select 
     *
 from tblAddressBook
     where hometown = '서울' and age > (select avg(age) from tblAddressBook where gender = 'm') and job not in('백수', '학생', '취업준비생');
 
--- tblAddressBook. gmail.com을 사용하는 사람들의 성별 > 세대별(10,20,30,40대) 인원수를 가져오시오.
+-- 7. tblAddressBook. gmail.com을 사용하는 사람들의 성별 > 세대별(10,20,30,40대) 인원수를 가져오시오.
 select 
     decode(gender, 'm', '남자', 'f', '여자') as 성별,
     count(decode(round(age/10), 1, 1)) as "10대",
@@ -39,26 +70,26 @@ from tblAddressBook
     group by gender;
 
 
--- tblAddressBook. 가장 나이가 많으면서 가장 몸무게가 많이 나가는 사람과 같은 직업을 가지는 사람들을 가져오시오.
+-- 8. tblAddressBook. 가장 나이가 많으면서 가장 몸무게가 많이 나가는 사람과 같은 직업을 가지는 사람들을 가져오시오.
 
 
 
 
--- tblAddressBook.  동명이인이 여러명 있습니다. 이 중 가장 인원수가 많은 동명이인(모든 이도윤)의 명단을 가져오시오.
+-- 9. tblAddressBook.  동명이인이 여러명 있습니다. 이 중 가장 인원수가 많은 동명이인(모든 이도윤)의 명단을 가져오시오.
 
 
 
--- tblAddressBook. 가장 사람이 많은 직업의(332명) 세대별 비율을 구하시오.
+-- 10. tblAddressBook. 가장 사람이 많은 직업의(332명) 세대별 비율을 구하시오.
 --    [10대]       [20대]       [30대]       [40대]
 --    8.7%        30.7%        28.3%        32.2%
 
 
 
--- tblStaff, tblProject. 현재 재직중인 모든 직원의 이름, 주소, 월급, 담당프로젝트명을 가져오시오.
+-- 11. tblStaff, tblProject. 현재 재직중인 모든 직원의 이름, 주소, 월급, 담당프로젝트명을 가져오시오.
 
        
        
--- tblVideo, tblRent, tblMember. '뽀뽀할까요' 라는 비디오를 빌려간 회원의 이름은?
+-- 12. tblVideo, tblRent, tblMember. '뽀뽀할까요' 라는 비디오를 빌려간 회원의 이름은?
 select * from tblVideo;
 select * from tblRent;
 select * from tblMember;
@@ -74,23 +105,23 @@ from tblMember m
                     where v.name = '뽀뽀할까요';
     
     
--- tblStaff, tblProejct. 'TV 광고'을 담당한 직원의 월급은 얼마인가?
+-- 13. tblStaff, tblProejct. 'TV 광고'을 담당한 직원의 월급은 얼마인가?
 
     
     
--- tblVideo, tblRent, tblMember. '털미네이터' 비디오를 한번이라도 빌려갔던 회원들의 이름은?
+-- 14. tblVideo, tblRent, tblMember. '털미네이터' 비디오를 한번이라도 빌려갔던 회원들의 이름은?
 
                 
--- tblStaff, tblProject. 서울시에 사는 직원을 제외한 나머지 직원들의 이름, 월급, 담당프로젝트명을 가져오시오.
+-- 15. tblStaff, tblProject. 서울시에 사는 직원을 제외한 나머지 직원들의 이름, 월급, 담당프로젝트명을 가져오시오.
 
         
--- tblCustomer, tblSales. 상품을 2개(단일상품) 이상 구매한 회원의 연락처, 이름, 구매상품명, 수량을 가져오시오.
+-- 16. tblCustomer, tblSales. 상품을 2개(단일상품) 이상 구매한 회원의 연락처, 이름, 구매상품명, 수량을 가져오시오.
 
                                 
--- tblVideo, tblRent, tblGenre. 모든 비디오 제목, 보유수량, 대여가격을 가져오시오.
+-- 17. tblVideo, tblRent, tblGenre. 모든 비디오 제목, 보유수량, 대여가격을 가져오시오.
           
                 
--- tblVideo, tblRent, tblMember, tblGenre. 2007년 2월에 대여된 구매내역을 가져오시오. 회원명, 비디오명, 언제, 대여가격
+-- 18. tblVideo, tblRent, tblMember, tblGenre. 2007년 2월에 대여된 구매내역을 가져오시오. 회원명, 비디오명, 언제, 대여가격
 select * from tblVideo;
 select * from tblRent;
 select * from tblMember;
@@ -129,7 +160,7 @@ select
 from tblTest
 where to_char(언제) like '07/02/%';
         
--- tblVideo, tblRent, tblMember. 현재 반납을 안한 회원명과 비디오명, 대여날짜를 가져오시오.
+-- 19. tblVideo, tblRent, tblMember. 현재 반납을 안한 회원명과 비디오명, 대여날짜를 가져오시오.
 select
     m.name as 회원명,
     v.name as 비디오명,
@@ -142,56 +173,56 @@ from tblMember m
                     where r.retdate is null;
     
     
--- employees, departments. 사원들의 이름, 부서번호, 부서명을 가져오시오.
+-- 20. employees, departments. 사원들의 이름, 부서번호, 부서명을 가져오시오.
 
         
         
--- employees, jobs. 사원들의 정보와 직업명을 가져오시오.
+-- 21. employees, jobs. 사원들의 정보와 직업명을 가져오시오.
 
         
         
--- employees, jobs. 직무(job_id)별 최고급여(max_salary) 받는 사원 정보를 가져오시오.
+-- 22. employees, jobs. 직무(job_id)별 최고급여(max_salary) 받는 사원 정보를 가져오시오.
 
       
     
     
--- departments, locations. 모든 부서와 각 부서가 위치하고 있는 도시의 이름을 가져오시오.
+-- 23. departments, locations. 모든 부서와 각 부서가 위치하고 있는 도시의 이름을 가져오시오.
 
         
         
--- locations, countries. location_id 가 2900인 도시가 속한 국가 이름을 가져오시오.
+-- 24. locations, countries. location_id 가 2900인 도시가 속한 국가 이름을 가져오시오.
 
             
             
--- employees. 급여를 12000 이상 받는 사원과 같은 부서에서 근무하는 사원들의 이름, 급여, 부서번호를 가져오시오.
+-- 25. employees. 급여를 12000 이상 받는 사원과 같은 부서에서 근무하는 사원들의 이름, 급여, 부서번호를 가져오시오.
 
         
         
--- employees, departments. locations.  'Seattle'에서(LOC) 근무하는 사원의 이름, 직위, 부서번호, 부서이름을 가져오시오.
+-- 26. employees, departments. locations.  'Seattle'에서(LOC) 근무하는 사원의 이름, 직위, 부서번호, 부서이름을 가져오시오.
 
     
     
--- employees, departments. first_name이 'Jonathon'인 직원과 같은 부서에 근무하는 직원들 정보를 가져오시오.
+-- 27. employees, departments. first_name이 'Jonathon'인 직원과 같은 부서에 근무하는 직원들 정보를 가져오시오.
 
     
     
--- employees, departments. 사원이름과 그 사원이 속한 부서의 부서명, 그리고 월급을 출력하는데 월급이 3000이상인 사원을 가져오시오.
+-- 28. employees, departments. 사원이름과 그 사원이 속한 부서의 부서명, 그리고 월급을 출력하는데 월급이 3000이상인 사원을 가져오시오.
 
             
             
--- employees, departments. 부서번호가 10번인 사원들의 부서번호, 부서이름, 사원이름, 월급을 가져오시오.
+-- 29. employees, departments. 부서번호가 10번인 사원들의 부서번호, 부서이름, 사원이름, 월급을 가져오시오.
 
             
             
--- departments, job_history. 퇴사한 사원의 입사일, 퇴사일, 근무했던 부서 이름을 가져오시오.
+-- 30. departments, job_history. 퇴사한 사원의 입사일, 퇴사일, 근무했던 부서 이름을 가져오시오.
 
         
         
--- employees. 사원번호와 사원이름, 그리고 그 사원을 관리하는 관리자의 사원번호와 사원이름을 출력하되 각각의 컬럼명을 '사원번호', '사원이름', '관리자번호', '관리자이름'으로 하여 가져오시오.
+-- 31. employees. 사원번호와 사원이름, 그리고 그 사원을 관리하는 관리자의 사원번호와 사원이름을 출력하되 각각의 컬럼명을 '사원번호', '사원이름', '관리자번호', '관리자이름'으로 하여 가져오시오.
 
         
         
--- employees, jobs. 직책(Job Title)이 Sales Manager인 사원들의 입사년도와 입사년도(hire_date)별 평균 급여를 가져오시오. 년도를 기준으로 오름차순 정렬.
+-- 32. employees, jobs. 직책(Job Title)이 Sales Manager인 사원들의 입사년도와 입사년도(hire_date)별 평균 급여를 가져오시오. 년도를 기준으로 오름차순 정렬.
 select * from employees;
 select * from jobs;
 
@@ -214,7 +245,7 @@ from employees e
 
 
 
--- employees, departments. locations. 각 도시(city)에 있는 모든 부서 사원들의 평균급여가 가장 낮은 도시부터 도시명(city)과 평균연봉, 해당 도시의 사원수를 가져오시오. 단, 도시에 근 무하는 사원이 10명 이상인 곳은 제외하고 가져오시오.
+-- 33. employees, departments. locations. 각 도시(city)에 있는 모든 부서 사원들의 평균급여가 가장 낮은 도시부터 도시명(city)과 평균연봉, 해당 도시의 사원수를 가져오시오. 단, 도시에 근 무하는 사원이 10명 이상인 곳은 제외하고 가져오시오.
 -- 미해결
 select * from employees;
 select * from departments;
@@ -229,12 +260,16 @@ from employees e
                 on d.location_id = l.location_id;
             
             
--- employees, jobs, job_history. ‘Public  Accountant’의 직책(job_title)으로 과거에 근무한 적이 있는 모든 사원의 사번과 이름을 가져오시오. 현재 ‘Public Accountant’의 직책(job_title)으로 근무하는 사원은 고려 하지 말것.
+-- 34. employees, jobs, job_history. ‘Public  Accountant’의 직책(job_title)으로 과거에 근무한 적이 있는 모든 사원의 사번과 이름을 가져오시오. 현재 ‘Public Accountant’의 직책(job_title)으로 근무하는 사원은 고려 하지 말것.
 
     
     
--- employees, departments, locations. 커미션을 받는 모든 사람들의 first_name, last_name, 부서명, 지역 id, 도시명을 가져오시오.
+-- 35. employees, departments, locations. 커미션을 받는 모든 사람들의 first_name, last_name, 부서명, 지역 id, 도시명을 가져오시오.
 
     
     
--- employees. 자신의 매니저보다 먼저 고용된 사원들의 first_name, last_name, 고용일을 가져오시오.
+-- 36. employees. 자신의 매니저보다 먼저 고용된 사원들의 first_name, last_name, 고용일을 가져오시오.
+
+
+
+
