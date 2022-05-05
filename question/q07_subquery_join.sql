853e5c65239e227c2740d77e208041bd90478e50
chickenpop <1005honeybadger@gmail.com>
2022-05-05 23:13:50 +0900

    q07 24~31

--- question/q07_subquery_join.sql
+++ question/q07_subquery_join.sql
@@ -1,6 +1,7 @@
 -- 22.04.30 4~5번 문제
 -- 22.05.01 8~11, 13~14문제
--- 22.05.04 15~17, 20~
+-- 22.05.04 15~17, 20~23
+-- 22.05.05 24~31
 
 -- 1. employees. 'Munich' 도시에 위치한 부서에 소속된 직원들 명단?
 -- 1. 결과
@@ -348,40 +349,118 @@ from employees e
     
     
 -- 23. departments, locations. 모든 부서와 각 부서가 위치하고 있는 도시의 이름을 가져오시오.
-
+select * from departments;
+select * from locations;
         
+select 
+    d.department_name, 
+    l.city
+from departments d
+    inner join locations l
+        on d.location_id = l.location_id;
         
 -- 24. locations, countries. location_id 가 2900인 도시가 속한 국가 이름을 가져오시오.
-
+select * from locations;
+select * from countries;
             
+select 
+    c.country_name as 국가명 
+from locations l
+    inner join countries c
+        on l.country_id = c.country_id
+            where l.location_id = 1900;
             
 -- 25. employees. 급여를 12000 이상 받는 사원과 같은 부서에서 근무하는 사원들의 이름, 급여, 부서번호를 가져오시오.
-
-        
-        
+select 
+    distinct e.department_id
+from employees e
+    inner join departments d
+        on e.department_id = d.department_id
+            where e.salary >= 12000;
+     
+select
+    first_name || ' ' || last_name as name, 
+    salary,
+    department_id
+from employees
+    where department_id in (select distinct e.department_id from employees e 
+                                                                inner join departments d
+                                                                    on e.department_id = d.department_id
+                                                                            where e.salary >= 12000);
+     
 -- 26. employees, departments. locations.  'Seattle'에서(LOC) 근무하는 사원의 이름, 직위, 부서번호, 부서이름을 가져오시오.
-
+select 
+    e.first_name || ' ' || e.last_name as 이름, 
+    e.job_id as 직위,
+    e.department_id as 부서번호,
+    d.department_name as 부서명
+from employees e
+    inner join departments d
+        on e.department_id = d.department_id
+            inner join locations l
+                on d.location_id = l.location_id
+                    where l.city = 'Seattle';
     
     
 -- 27. employees, departments. first_name이 'Jonathon'인 직원과 같은 부서에 근무하는 직원들 정보를 가져오시오.
-
+select 
+    department_id
+from employees 
+    where lower(first_name) like 'jonathon';
     
+select 
+    *
+from employees
+    where department_id = (select 
+                                department_id
+                            from employees 
+                                where lower(first_name) like 'jonathon');
+
     
 -- 28. employees, departments. 사원이름과 그 사원이 속한 부서의 부서명, 그리고 월급을 출력하는데 월급이 3000이상인 사원을 가져오시오.
-
+select 
+    e.first_name || ' ' || e.last_name as 이름,
+    d.department_name as 부서명,
+    e.salary as 월급
+from employees e
+    inner join departments d
+        on e.department_id = d.department_id
+            where e.salary >= 3000;
             
             
 -- 29. employees, departments. 부서번호가 10번인 사원들의 부서번호, 부서이름, 사원이름, 월급을 가져오시오.
-
-            
+select 
+    d.department_id,
+    d.department_name,
+    e.first_name || ' ' || e.last_name as name,
+    e.salary
+from employees e
+    inner join departments d
+        on e.department_id = d.department_id
+            where e.department_id = 10;
             
 -- 30. departments, job_history. 퇴사한 사원의 입사일, 퇴사일, 근무했던 부서 이름을 가져오시오.
-
+select * from departments;
+select * from job_history;
         
+select 
+    j.start_date,
+    j.end_date,
+    d.department_name
+from job_history j
+    inner join departments d
+        on j.department_id = d.department_id;
         
 -- 31. employees. 사원번호와 사원이름, 그리고 그 사원을 관리하는 관리자의 사원번호와 사원이름을 출력하되 각각의 컬럼명을 '사원번호', '사원이름', '관리자번호', '관리자이름'으로 하여 가져오시오.
-
-        
+select 
+    e.employee_id as 사원번호,
+    e.first_name || ' ' || e.last_name as 사원이름,
+    e.manager_id as 관리자번호,
+    m.first_name || ' ' || m.last_name as 관리자이름
+from HR.employees e
+    inner join HR.employees m
+        on e.manager_id = m.employee_id ;
+    
         
 -- 32. employees, jobs. 직책(Job Title)이 Sales Manager인 사원들의 입사년도와 입사년도(hire_date)별 평균 급여를 가져오시오. 년도를 기준으로 오름차순 정렬.
 select * from employees;
