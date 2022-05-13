4c2f8464d02330ce1d0ef1aed3b5606ff4e6fb50
chickenpop <1005honeybadger@gmail.com>
2022-05-13 23:41:31 +0900

    t03 ddl create & constraint pk, uq, ck

---
+++ training/t03_ddl.sql
@@ -0,0 +1,42 @@
+-- create와 제약사항
+
+drop table tblConstraint;
+drop sequence seqCons;
+
+-- constraint로 만들기
+create table tblConstraint(
+-- null은 적지 않으면 기본값
+-- not null, null, default는 컬럼수준에서 가능
+    seq number(3),
+    name varchar2(15) not null,
+    nickname varchar2(30) null,
+    memo varchar2(300) null, 
+    regdate date default sysdate,
+    
+    --constraint tblConstraint_seq_pk primary key(seq),
+    constraint tblConstraint_nickname_uq unique(nickname),
+    constraint tblConstraint_memo_ck check(length(memo) > 0)
+
+);
+
+-- alter로 제약사항 추가하기
+alter table tblConstraint
+    add constraint tblConstraint_seq_pk primary key(seq);
+
+create sequence seqCons;
+
+select * from tblConstraint;
+
+insert into tblConstraint (seq, name, nickname, memo) values (seqCons.nextVal, '홍길동', 'hijava', 'hi~');
+-- ORA-00001: unique constraint (HR.TBLCONSTRAINT_NICKNAME_UQ) violated
+-- 의도적 삽입 > 유니크 특성 : 중복값 불가, null가능 
+insert into tblConstraint (seq, name, nickname, memo) values (seqCons.nextVal, '아무개', 'hijava', 'hi~');
+insert into tblConstraint (seq, name, memo) values (seqCons.nextVal, '홍길동', 'hi~');
+insert into tblConstraint (seq, name, nickname, memo) values (seqCons.nextVal, '홍길동', 'hong', 'hi~');
+
+
+-- alter 제약사항 확인
+-- ORA-00001: unique constraint (HR.TBLCONSTRAINT_SEQ_PK) violated
+insert into tblConstraint (seq, name, nickname, memo) values (1, '홍길동', 'hijava', 'hi~');
+insert into tblConstraint (seq, name, nickname, memo) values (1, '홍길동', 'hong', 'hi~');
+
