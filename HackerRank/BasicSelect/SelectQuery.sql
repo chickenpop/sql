a1b3061486d30253009864439802c17655d209b1
chickenpop <1005honeybadger@gmail.com>
2022-09-30 19:30:43 +0900

    HACKERRANK BASIC SELECT
    Higher Than 75 Marks, Employee Names 와 Salaries, African Cities, Average Population of Each Continent

--- HackerRank/BasicSelect/SelectQuery.sql
+++ HackerRank/BasicSelect/SelectQuery.sql
@@ -18,3 +18,30 @@ SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN';
 
 -- Weather Observation Station 1
 SELECT CITY, STATE FROM STATION;
+
+-- Higher Than 75 Marks
+-- marks가 75보다 크고, 마지막 문자열 3개가 동일한 경우 ID로 정렬
+SELECT NAME FROM STUDENTS WHERE MARKS > 75 ORDER BY RIGHT(NAME, 3), ID; 
+
+-- Employee Names
+-- 직원 이름 정렬
+SELECT NAME FROM EMPLOYEE ORDER BY NAME;
+
+-- Employee Salaries
+-- 월급 2000 이상 10개월 미만인 직원
+SELECT NAME FROM EMPLOYEE WHERE SALARY >= 2000 AND MONTHS < 10 ORDER BY EMPLOYEE_ID;
+
+-- African Cities
+-- 아프리카 대륙 있는 도시 이름
+SELECT C1.NAME 
+  FROM CITY C1 
+ INNER JOIN COUNTRY C2 ON C1.COUNTRYCODE = C2.CODE 
+ WHERE C2.CONTINENT = 'Africa';
+
+-- Average Population of Each Continent
+-- 각 대륙의 평균 인구수를 내림해서 출력
+SELECT C2.CONTINENT 
+     , FLOOR(AVG(C1.POPULATION)) 
+  FROM CITY C1 
+ INNER JOIN COUNTRY C2 ON C1.COUNTRYCODE = C2.CODE
+ GROUP BY C2.CONTINENT;
